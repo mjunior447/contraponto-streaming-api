b7541f37d48258439064c3a17cd34343d4ab75fa
Mauricio Antero <mjunior447@gmail.com>
2026-06-30 10:31:48 -0300

    feat: configure video upload route

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const adminAuth = (req, res, next) => {
     const apiKey = req.headers["x-api-key"];
 
-    if (!apikey || apikey !== process.env.ADMIN_API_KEY) {
+    if (!apiKey || apiKey !== process.env.ADMIN_API_KEY) {
         return res.status(401).json({
             error: "Acesso não autorizado. Chave de API inválida"
         });
